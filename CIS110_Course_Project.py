1225398e46ec062845594f264152e3c0b6ae024b
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-04-27 21:11:07 -0700

    Started project

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -1,1 +1,19 @@
-print("Hello World")
+print("Welcome! I am glad you made it! I need a little help from you today.")
+print("Before we get into it, I need to get some info from you.")
+print("I will ask you a few questions, just make sure you press enter after you answer, and we will be on our way!")
+input("\nPress the enter key to continue...")
+fav_color = input("\nWhat is your favorite color:   ")
+fav_game = input("\nWhat is your favorite game:   ")
+player_name = input("\nWhat is your name:   ")
+phone_brand = input("\nWhat is your favorite phone:   ")
+country_choice = input("\nWhat country would you want to visit:   ")
+hieght_type = input("\nChoose either tall or short:   ")
+sky_choice = input("\nDo you prefer rainbows or shooting stars:   ")
+
+print("\nHERE WE GO!!!")
+
+print("\nThe princess is playing", fav_game, "on her", phone_brand, )
+print("In the royal garden of", country_choice, "when out of the blue a storm starts raging!")
+print("You can see the", fav_color, "lightning all around. The wind is relentless and the rain drops are as large as GEESE EGGS!")
+print("She has to decide where to go and fast!")
+
